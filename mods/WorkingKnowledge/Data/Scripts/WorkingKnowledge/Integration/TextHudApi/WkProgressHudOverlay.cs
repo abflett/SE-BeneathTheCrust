d0cc70f27614149360f0144c8744bebdc4100400
Adam Flett <abflett@yahoo.ca>
2026-07-08 17:17:47 +0800

    Mask Working Knowledge HUD bar seam

--- mods/WorkingKnowledge/Data/Scripts/WorkingKnowledge/Integration/TextHudApi/WkProgressHudOverlay.cs
+++ mods/WorkingKnowledge/Data/Scripts/WorkingKnowledge/Integration/TextHudApi/WkProgressHudOverlay.cs
@@ -21,7 +21,7 @@ namespace WkKn
         private const double AnchorY = 0.78;
         private const double BarLeftX = 0.49;
         private const double ResearchBarY = -0.028;
-        private const double ProficiencyBarY = -0.042;
+        private const double ProficiencyBarY = -0.040;
         private const double BarWidth = 0.40;
         private const double BarHeight = 0.016;
         private const double ProficiencyBarHeight = 0.0112;
